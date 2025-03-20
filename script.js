84c323000dae3ead669eb8626fbcbf6e98bb4f3e
kertifyNu <126778369+kertifyNu@users.noreply.github.com>
2025-03-21 02:10:59 +0500

    Add password reset functionality and user feedback modal

--- script.js
+++ script.js
@@ -33,7 +33,44 @@ if (signForm) {
     document.getElementById("infoModal").style.display = "block";
   });
 }
+const resetForm = document.getElementById("reset-form");
+if (resetForm) {
+  resetForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+    const userInfoDiv = document.getElementById("userInfo");
+    if (password !== confirmPassword) {
+      userInfoDiv.innerHTML = `       
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Password:</strong> ${password}</p>
+        <p><strong>Confirm Password:</strong> ${confirmPassword}</p>
+        <p>Passwords do not match</p>
+    `;
+      document.getElementById("infoModal").style.display = "block";
+      return;
+    }
+    if (password.length < 6 || confirmPassword.length < 6) {
+      userInfoDiv.innerHTML = `       
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Password:</strong> ${password}</p>
+        <p><strong>Confirm Password:</strong> ${confirmPassword}</p>
+        <p>Password must be at least 6 characters</p>
+    `;
+      document.getElementById("infoModal").style.display = "block";
+      return;
+    }
 
+    userInfoDiv.innerHTML = `       
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Password:</strong> ${password}</p>
+        <p><strong>Confirm Password:</strong> ${confirmPassword}</p>
+    `;
+    console.log(email);
+    document.getElementById("infoModal").style.display = "block";
+  });
+}
 const closeBtn = document.getElementById("close");
 if (closeBtn) {
   closeBtn.addEventListener("click", (e) => {
